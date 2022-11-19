2c1f4dacfe72be774be15ac41125bb9f3328a421
Ro_Room <mikhi2800@gmail.com>
2022-11-18 20:16:36 -0500

    Doc

---
+++ doc/midterm_extracredit/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["main","Main Function"]],"struct":[["City","Demonstrates printing of a user defined struct using println! macro. Struct for City_location"],["Color","Struct for Colors"]]};
